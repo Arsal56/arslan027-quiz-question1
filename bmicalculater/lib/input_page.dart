4b77fdaefe0bb268fcbe8c3529c5fd67938b2bbf
Muhammad Asad <100906488+Arsal56@users.noreply.github.com>
2022-11-10 11:26:50 +0500

    add font file in

--- bmicalculater/lib/input_page.dart
+++ bmicalculater/lib/input_page.dart
@@ -1,6 +1,6 @@
 import 'package:bmi_calculator/constant.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'calculator_file.dart';
 import 'container_file.dart';
