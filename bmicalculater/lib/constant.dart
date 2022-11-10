1a6efb5a6027b460a721c138c863a277c6066cee
Muhammad Asad <100906488+Arsal56@users.noreply.github.com>
2022-11-10 11:25:16 +0500

    update constants

--- bmicalculater/lib/constant.dart
+++ bmicalculater/lib/constant.dart
@@ -12,3 +12,28 @@ fontSize: 50.0,
 fontWeight: FontWeight.w900,
 );
 
+const kLargeButtonStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleStyleS2 = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultText = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.green,
+);
+
+const kBMiTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kbodyTextStyle = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+);
