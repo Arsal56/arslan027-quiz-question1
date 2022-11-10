8d5680122f36449e9db4d628bbd8ce233e6ed6ce
Muhammad Asad <100906488+Arsal56@users.noreply.github.com>
2022-11-10 11:22:31 +0500

    COntainer file make

---
+++ bmicalculater/lib/container_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RepeatContainerCode extends StatelessWidget {
+  RepeatContainerCode(
+      {required this.colors,
+      required this.CardWidget,
+      required this.onPressed});
+  final Color colors;
+  final Widget CardWidget;
+  void Function()? onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
